dc3876d110a5b6b5238c76187bbcbe4a0ee7204e
Lihong Dang <Chrisdang>
2025-11-20 00:23:24 -0500

    delete middleware

--- App/main.py
+++ App/main.py
@@ -8,15 +8,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins=["https://www.google.com","https://fastapi.tiangolo.com/"]
-models.Base.metadata.create_all(bind=engine)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins=["https://www.google.com","https://fastapi.tiangolo.com/"]
+# models.Base.metadata.create_all(bind=engine)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(post.router)
 app.include_router(user.router)
